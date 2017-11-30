b25d959d923751306075f1c891080e8c79229686
Harald Göttlicher <harryg@harryg.de>
2017-11-30 18:33:00 +0100

    Rewrite stable and faster working with any size and also tested with 2 separate clients.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # juce_projects
+Some example projects for JUCE that you might find useful.
+
+## test-osc
+A tool to test the max. blob size in OSC messages on your network setup (either on one or two machines).
--- Test-OSC/Source/Main.cpp
+++ Test-OSC/Source/Main.cpp
@@ -1,119 +1,265 @@
 #include "../JuceLibraryCode/JuceHeader.h"
-
-// start size of blobs sent via OSC
-#define START_TEST_SIZE_BYTES 4000
-
 /**
-    Tests the max. safe size of blobs via OSC.
+    Copyright (c) 2017 Harry G.
+    Permission is granted to use this software under the terms of the GPL v3 (or any later version)
+    Details of these licenses can be found at: www.gnu.org/licenses
 
-    Start without parameters to test on this machine (send & receive).
+    This tool tests the max. safe size of blobs via OSC send over UDP.
+
+    Gueranteed in IPv4 Spec is only 576, see https://tools.ietf.org/html/rfc791 (search for "Total Length" or "576")
+    IPv4 protocol would allow 65,507 bytes https://en.wikipedia.org/wiki/User_Datagram_Protocol#Packet_structure
+
+    Start without parameters to test on one machine (send & receive).
 
     Use command line options to test over a network:
-    -ip=<address> ip-address (not name!) of the other machine - defaults to 127.0.0.1
-    -port=<port> port of the other machine - defaults to 9000
-    -bs=<size> the blob size to start the test with - defaults to 4000, which works in most cases. Gueranteed in IPv4 Spec is only 576!
-    -s send only - you have to start the receiver first for this to work!
-    -r receive only
+     -s send mode - you have to start the receiver first for this to work!
+     -r receive mode
     Default is both -s and -r to do both send and receive in parallel on one machine.
+    Optionally, use different ports and IP:
+     -ip=<address> ip-address or name of the other machine - defaults to 127.0.0.1
+     -p=<port> port of the other machine - defaults to 9000
+     -fb=<port> port for feedback to sender - defaults to 9001
 */
-class TestOSC : public JUCEApplication, OSCReceiver, OSCReceiver::Listener<>, Timer {
+
+// start increment size of blobs sent via OSC
+#define START_INCREMENT_BYTES 10000
+class TestOSCSender : public OSCReceiver, OSCReceiver::Listener<>, Timer {
 public:
-    TestOSC() {
-        // check command line options
-        StringArray params = getCommandLineParameterArray();
-        for (auto param : params) {
-            if (param.startsWithIgnoreCase("-ip="))
-                ip = param.substring(4);
-            if (param.startsWithIgnoreCase("-port="))
-                port = param.substring(6).getIntValue();
-            if (param.startsWithIgnoreCase("-s"))
-                send = true;
-            if (param.startsWithIgnoreCase("-r"))
-                receive = true;
-            if (param.startsWithIgnoreCase("-bs="))
-                size = param.substring(4).getIntValue();
-        }
-
-        // no option given? send & receive
-        if (!send && !receive) {
-            printf("Using default option send & receive on one machine.\n");
-            send = true;
-            receive = true;
-        }
-
-        if (receive) {
-            printf("Connecting OSC receiver...\n");
-            jassert(connect(port));
-            // listen for OSC messages
-            addListener(this);
-        }
-
-
-        if (send) {
+    TestOSCSender(String ip, int port, int fbPort) {
             printf("Connecting OSC sender...\n");
             jassert(sender.connect(ip, port));
-            // initial size
-            data.ensureSize(size);
+
+            // sender listens for feedback
+            addListener(this);
+            jassert(connect(fbPort));
+
             // sender timer
             startTimerHz(10);
-        }
     }
 
-    ~TestOSC() {
-        stopTimer();
+    bool isActive() {
+        return active;
     }
-    
+
+    int getLastSize() {
+        return sizeToSend;
+    }
+
 private:
     MemoryBlock data;
     OSCSender sender;
-    int32 size = START_TEST_SIZE_BYTES, counterSender = 0, counterReceiver = 0, counterBlob = 0, blobSize = 0;
-    bool send = false, receive = false, stop = false;
-    String ip = "127.0.0.1";
-    int port = 9000;
+    bool active = true, success = true;
+    int increment = START_INCREMENT_BYTES;
+    int sizeToSend = 1;
+
+    void setSuccess() {
+        success = true;
+    }
+
+    void handleFailed() {
+        // go back with current increment
+        sizeToSend -= increment * 2;
+        // retry with 10-times smaller increment if not yet incremented in steps of 1
+        if (increment > 1) {
+            increment /= 10;
+            printf("Received error - retrying with increment by %d \n", increment);
+            sizeToSend++;
+        }
+    }
+
+    void oscMessageReceived(const OSCMessage& message) override
+    {
+        String address = message.getAddressPattern().toString();
+        if (address.contains("success")) {
+            setSuccess();
+        }
+        if (address.contains("failed")) {
+            handleFailed();
+        }
+        if (address.contains("stop")) {
+            printf("Received stop.\n");
+            stopTimer();
+            sizeToSend -= increment * 2;
+            active = false;
+        }
+    }
 
     virtual void timerCallback() override
     {
-        // send counter and blob
-        sender.send("/test/counter", ++counterSender);
-        printf("Sending blob    : %d\n", counterSender);
-        jassert(sender.send("/test/data", data));
-        // increase size by 1
-        data.ensureSize(++size);
+        if (success) {
+            // increase size
+            data.reset();
+            data.ensureSize(sizeToSend);
+            // send counter and blob
+            printf("Sending blob with size  : %d \n", data.getSize());
+            jassert(sender.send("/test/data", data));
+            sender.send("/test/size", sizeToSend);
+
+            sizeToSend += increment;
+        }
     }
+};
+
+class TestOSCReceiver : public OSCReceiver, OSCReceiver::Listener<> {
+public:
+    TestOSCReceiver(String ip, int port, int fbPort) {
+        // listen for OSC messages
+        addListener(this);
+
+        printf("Connecting OSC receiver...\n");
+        jassert(connect(port));
+        jassert(fbSender.connect(ip, fbPort));
+    }
+
+    bool isActive() {
+        return active;
+    }
+
+    int getLastSize() {
+        return sizeReceived;
+    }
+
+private:
+    OSCSender fbSender;
+    int32  sizeExpected = 0, sizeReceived = 0;
+    bool active = true;
 
     void oscMessageReceived(const OSCMessage& message) override
     {
         jassert(message.size() == 1);
         // receive counter
         if (message[0].isInt32()) {
-            printf("Received counter: %s - value: %d\n", message.getAddressPattern().toString().getCharPointer(), counterSender);
+            int32 lastIncrement = message[0].getInt32() - sizeExpected;
+            sizeExpected = message[0].getInt32();
+            printf("Received expected size  : %d ... ", sizeExpected);
             // before evaluating next counter, check if corresponding blob was already received
-            if (counterBlob != counterReceiver) {
-                stopTimer();
-                // missing blob - show last successful size
-                printf("Detected missing blob!\n");
-                printf("\nMax. blob size in this setup is %d bytes\n", blobSize);
-                printf("\nPress Enter to exit...\n");
-                getchar();
-                quit();
+            if (sizeExpected != sizeReceived) {
+                // if not yet incremented in steps of 1
+                if (lastIncrement > 1) {
+                    // give error feedback for retry
+                    printf("Detected missing blob! Sending Retry.\n");
+                    fbSender.send("/test/failed");
+                }
+                // incremented in steps of 1? That's it!
+                else {
+                    printf("Detected missing blob! Sending Stop.\n");
+                    fbSender.send("/test/stop");
+                    active = false;
+                }
             }
-            // store recieved counter
-            counterReceiver = message[0].getInt32();
-        // receive blob
-        } else if (message[0].isBlob()) {
-            blobSize = message[0].getBlob().getSize();
-            printf("Received blob   : %s    - size : %d\n", message.getAddressPattern().toString().getCharPointer(), blobSize);
-            // count received blobs
-            counterBlob++;
-        } else
+            else {
+                // successfully received expected size, request next one
+                printf("OK!\n");
+                fbSender.send("/test/success");
+            }
+            // receive blob
+        }
+        else if (message[0].isBlob()) {
+            sizeReceived = message[0].getBlob().getSize();
+            printf("Received blob with size : %d\n", sizeReceived);
+        }
+        else
             jassert(false);
     }
+};
+
+class TestOSC : public JUCEApplication, Timer {
+public:
+    TestOSC() {
+        printf("TestOSC\n=======\nTest tool for testing max. OSC blob size in a network.\n\n");
+
+        // check command line options
+        StringArray params = getCommandLineParameterArray();
+        if (params.size() == 0) {
+            // show comandline help, but run anyway with defaults
+            printf("Comandline parameters are:\n");
+            printf(" -s - send mode - you have to start the receiver first for this to work!\n");
+            printf(" -r - receive mode\n");
+            printf("Default is both - s and -r to do both send and receive in parallel on one machine.\n");
+            printf("Optionally, use different ports and IP:\n");
+            printf(" -ip<address> ip-address or name of the other machine - defaults to 127.0.0.1\n");
+            printf(" -p<port> port of the other machine - defaults to 9000\n");
+            printf(" -fb<port> port for feedback to sender - defaults to 9001\n");
+            printf("Running wit default parameters now...\n\n");
+        }
+        for (auto param : params) {
+            if (param.startsWithIgnoreCase("-ip")) {
+                ip = param.substring(3);
+                printf("Using IP %s\n", ip);
+            }
+            if (param.startsWithIgnoreCase("-p")) {
+                port = param.substring(2).getIntValue();
+                printf("using port %d\n", port);
+            }
+            if (param.startsWithIgnoreCase("-fb")) {
+                fbPort = param.substring(3).getIntValue();
+                printf("using feedback port %d\n", fbPort);
+            }
+            if (param.startsWithIgnoreCase("-s")) {
+                printf("Send mode selected\n");
+                send = true;
+            }
+            if (param.startsWithIgnoreCase("-r")) {
+                printf("Receive mode selected\n");
+                receive = true;
+            }
+        }
+
+        // no option given? send & receive
+        if (!send && !receive) {
+            printf("Using default option send & receive on one machine.\n\n");
+            send = true;
+            receive = true;
+        }
+
+        if (send)
+            sender = new TestOSCSender(ip, port, fbPort);
+        if (receive)
+            receiver = new TestOSCReceiver(ip, port, fbPort);
+        printf("\n");
+
+        startTimerHz(10);
+    }
+
+    ~TestOSC() {
+        if (send)
+            delete sender;
+        if (receive)
+            delete receiver;
+    }
+
+private:
+    TestOSCSender* sender;
+    TestOSCReceiver* receiver;
+    bool send = false, receive = false;
+    String ip = "127.0.0.1";
+    int port = 9000, fbPort = 9001;
 
     // Inherited via JUCEApplication - not needed here
     virtual const String getApplicationName() override { return String(); }
     virtual const String getApplicationVersion() override { return String(); }
     virtual void initialise(const String & ) override {}
     virtual void shutdown() override {}
+
+    // Inherited via Timer
+    virtual void timerCallback() override
+    {
+        if (send && !sender->isActive() || receive && !receiver->isActive()) {
+            // show last successful size
+            int size;
+            // prefered is received size
+            if (receive)
+                size = receiver->getLastSize();
+            // only as info on sender side if running standalone
+            else
+                size = sender->getLastSize();
+            printf("\nRESULT: Max. safe OSC blob size in this setup is %d bytes\n", size);
+            printf("\nPress Enter to exit...\n");
+            getchar();
+            stopTimer();
+            quit();
+        }
+    }
 };
 
 START_JUCE_APPLICATION(TestOSC)
